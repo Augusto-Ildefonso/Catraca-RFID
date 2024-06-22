1d09b77309dfb44e6d4c2d5f628bde3958d10545
Marco Tulio <marcotulio270903@gmail.com>
2024-06-22 15:50:05 -0300

    Função arduino e Readme

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 Como não foi possível montar no Tinkercad o circuito, pois não havia o componente da placa RFID, encontramos esta imagem que ilustra as ligações da placa com o arduino. Vale ressaltar que no projeto a placa foi encaixada na protoboard e as conexṍes foram feitas através dela.
 
   
-![Circuito do Arduino com a placa RFID](https://github.com/Augusto-Ildefonso/Catraca-RFID/blob/72c2508d2b34d03063236a011ec34842c37524b2/circuito_rfid.jpg)
+![Circuito do Arduino com a placa RFID](./figs/circuito_rfid.jpg)
 ###### Crédito: [blogmasterworkshop](https://blogmasterwalkershop.com.br/wp-content/uploads/2018/06/img01_como_usar_com_arduino_-_kit_rfid_mfrc522_uno_mega_2560_nano_controle_acesso_automacao_residencial.jpg)
 
 Segue agora o circuito das ligações dos leds com o arduino. Tanto esse circuito quanto o acima foram feitos na mesma protoboard.
 
-![Circuito do Arduino com os leds](https://github.com/Augusto-Ildefonso/Catraca-RFID/blob/c24472b30529c9aab2e894c9be5a93c008bae57f/circuito_led.png)
+![Circuito do Arduino com os leds](./figs/circuito_led.png)
 
 ## Imagens do projeto
 
--- src/main.ino
+++ src/main.ino
@@ -1,68 +1,85 @@
-#include <MFRC522.h> 
-#include <SPI.h>    
-#include <Wire.h>   
+#include <MFRC522.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-
-const int led_verde = 3;    
+int cont = 1;
+char id_org[12];
+const int led_verde = 3;
 const int led_vermelho = 2;
-const int tempo_delay = 3000;
-const char* tag_valida = "TAG";
+const int led_teste = 8;
+const int tempo_delay = 1000;
+const char *tag_valida = "A3:79:B1:0E";
 void setup() {
-
+  int cond = 1;
   // Inicializando biblioteca, barramento SPI e MFRC522
-  Wire.begin();   
-  SPI.begin();     
+  Wire.begin();
+  SPI.begin();
   rfid.PCD_Init();
-
-  pinMode(led_verde, OUTPUT);   
+  pinMode(led_verde, OUTPUT);
   pinMode(led_vermelho, OUTPUT);
-
+  pinMode(led_teste, OUTPUT);
+  digitalWrite(led_teste, LOW);
   digitalWrite(led_verde, LOW);
   digitalWrite(led_vermelho, LOW);
+  // formatID(rfid.uid.uidByte, id_org);
 }
 
 /**
-* Função principal
-*/
+ * Função principal
+ */
 void loop() {
+  digitalWrite(led_teste, HIGH);
+  while (cont) {
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+      return;
+
+    formatID(rfid.uid.uidByte, id_org);
+
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+    cont = 0;
+  }
+  digitalWrite(led_teste, LOW);
   leituraRfid();
 }
 
-char* formatID(byte *buffer, char *output) {
+char *formatID(byte *buffer, char *output) {
+
   for (byte i = 0; i < 4; i++) {
-    sprintf(&output[i*3], "%02X:", buffer[i]);
+    sprintf(&output[i * 3], "%02X:", buffer[i]);
   }
   output[11] = '\0';
+
   return output;
 }
 
 /*
-* Essa função realiza a leitura do cartão RFID
-*/
+ * Essa função realiza a leitura do cartão RFID
+ */
 void leituraRfid() {
-  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()){
-    return; 
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    return;
   }
-  
+
   // Gera a tag RFID lida
   char id[12];
   formatID(rfid.uid.uidByte, id);
-  
-if (strstr(strID, tag_valida) != NULL) {
-    digitalWrite(led_vermelho, HIGH);
-    delay(DELAY_TIME);
+
+  if (strstr(id, id_org) != NULL) {
+    digitalWrite(led_verde, HIGH);
+    delay(tempo_delay);
     digitalWrite(led_verde, LOW);
   } else {
-    digitalWrite(led_verde, HIGH);
+    digitalWrite(led_vermelho, HIGH);
     delay(tempo_delay);
     digitalWrite(led_vermelho, LOW);
   }
 
   // Para leitura no cartão e criptografia no PCD
-  rfid.PICC_HaltA();      
+  rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
 }
